7787467686d80a092df938eeb59801cc8f60f979
caroolnascimento <72581434+caroolnascimento@users.noreply.github.com>
2021-02-10 18:56:48 -0300

    Inserindo Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 
-import ComponentMenu from "./components/ComponentMenu";
-import ComponentFooter from "./components/ComponentFooter";
-import ComponentRouters from './components/ComponentRouters';
+import { lazy, Suspense } from "react";
+//import ComponentMenu from "./components/ComponentMenu";
+//import ComponentFooter from "./components/ComponentFooter";
+//import ComponentRouters from './components/ComponentRouters';
+
+const ComponentMenu = lazy(()=> import('./components/ComponentMenu'));
+const ComponentFooter = lazy(()=> import('./components/ComponentFooter'));
+const ComponentRouters = lazy(()=> import('./components/ComponentRouters'));
+
 
 function App() {
   return (
     <HashRouter>
       <div className="container-fluid">
+        <Suspense fallback={<p>CARREGANDO...</p>}>
         <ComponentMenu />
+        </Suspense>
+        <Suspense fallback={<p>CARREGANDO...</p>}>
         <ComponentRouters />
+        </Suspense>
+        <Suspense fallback={<p>CARREGANDO...</p>}>
         <ComponentFooter />
+        </Suspense>
       </div>
     </HashRouter>
   )
